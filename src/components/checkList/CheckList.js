8987e2d8b54fa5f1b37e2a2c8a167adfd72d1907
mbaye-eulerity <mbaye.faye@eulerity.com>
2020-10-18 19:44:57 -0400

    add input to card

--- src/components/checkList/CheckList.js
+++ src/components/checkList/CheckList.js
@@ -2,14 +2,21 @@ import React from 'react';
 import './style.css';
 const CheckList = ({ cardId, tasks }) => {
   return (
-    <div>
-      {tasks.map((task) => (
-        <li className='checkList_task' key={cardId}>
-          <input type='checkbox' defaultChecked={task.done} />
-          {task.name}
-          <a href='#che' className='checkList_task--remove' />
-        </li>
-      ))}
+    <div className='checklist'>
+      <ul>
+        {tasks.map((task) => (
+          <li className='checkList_task' key={cardId}>
+            <input type='checkbox' defaultChecked={task.done} />
+            {task.name}
+            <a href='#che' className='checkList_task--remove' />
+          </li>
+        ))}
+      </ul>
+      <input
+        type='text'
+        className='checkList--add-task'
+        placeholder='type then hit enter to add task'
+      />
     </div>
   );
 };
